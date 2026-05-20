a7499c591eff388925b6d861dc9cc621344f58de
sadukhyoobbenry-prog <sadukhyoobbenry@gmail.com>
2026-05-20 14:34:45 +0700

    Update FilterSection.tsx

--- src/components/harga-komoditas/FilterSection.tsx
+++ src/components/harga-komoditas/FilterSection.tsx
@@ -101,7 +101,7 @@ FilterSection({
         <div className="flex flex-col md:flex-row gap-4">
 
           {/* KOMODITAS */}
-          <div className="w-full min-w-0">
+          <div className="w-full md:min-w-[320px]">
 
             <label className="block text-sm font-medium mb-2">
               Pilih Komoditas
